7cf5b793682db94d409f3a42c7b315b5f7f82353
Matthew Samushonok <matthew.samushonok@gmail.com>
2020-12-11 18:28:34 +0200

    Adding app files to repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const App = () => {
+
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <h3>From the Latvia</h3>
+        </div>
+    )
+}
+
